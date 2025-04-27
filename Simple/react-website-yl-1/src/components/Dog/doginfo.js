e105776049950fe2a37459fa35d1641041ce114f
Chandugoud-khammampati <kchandugoud09@gmail.com>
2025-04-27 18:36:20 -0500

    added

--- Simple/react-website-yl-1/src/components/Dog/doginfo.js
+++ Simple/react-website-yl-1/src/components/Dog/doginfo.js
@@ -1,5 +1,10 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 jgefuiewgofiuewgoifugewoufgewuofg
 =======
 uagcddsukfbcdusbcksjdbvcdjslvhjd,cvyjc,yvdshcjdsvfhjdsvvmsjmdsvcvdcvjvhdsj
 >>>>>>> 306d46a7fb09d6e0da9cb27fe31334526e2c800c
+=======
+
+uagcddsukfbcdusbcksjdbvcdjslvhjd,cvyjc,yvdshcjdsvfhjdsvvmsjmdsvcvdcvjv
+>>>>>>> Stashed changes
---
+++ Simple/react-website-yl-1/src/components/butterfly/butterflys.js
@@ -0,0 +1,24 @@
+// ButterflyInfo.js
+import React from 'react';
+
+const ButterflyInfo = () => {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <img 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Monarch_In_May.jpg/640px-Monarch_In_May.jpg" 
+        alt="Butterfly"
+        style={{ width: '300px', borderRadius: '8px' }}
+      />
+      <h2>Monarch Butterfly</h2>
+      <p>
+        The Monarch butterfly is known for its long migration from North America to central Mexico. 
+        It is one of the most beautiful and recognizable butterflies with orange, black, and white patterns.
+      </p>
+    </div>
+  );
+};
+
+export default ButterflyInfo;
+
+
+
